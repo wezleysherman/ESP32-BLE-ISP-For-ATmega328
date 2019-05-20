5c6aaba90dad707e9d06fde2069b317bb4b3237b
Tanner Lisonbee <lisonbeetanner@gmail.com>
2019-05-20 14:37:50 -0700

    Fixed a couple issues in the flash script so it works properly now.

--- buildscripts/flash.sh
+++ buildscripts/flash.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 if [ "$1" == "bootloader" ]; then
-	if [ "$2" == 0 ]; then
-	    rmdir Trynkit-Firmware-Updater
+    if [[ $2 -eq 0 ]]; then
+        rmdir Trynkit-Firmware-Updater
         git clone https://github.com/wezleysherman/Trynkit-Firmware-Updater
-        python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 /root/TrynkitEsp32ISP/bootloader.bin
-	else
-	    python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 $2
+        python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 /root/Trynkit-Firmware-Updater/bootloader.bin
+    else
+        python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 $2
     fi
 elif [ "$1" == "partitiontable" ]; then
-    if [ "$2" == 0 ]; then
+    if [[ $2 -eq 0 ]]; then
         rmdir TrynkitEsp32ISP
         git clone https://github.com/wezleysherman/TrynkitEsp32ISP
         python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x8000 /root/TrynkitEsp32ISP/binary_partitions.bin
-	else
-	    python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x8000 $2
+    else
+        python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x8000 $2
     fi
 elif [ "$1" == "updater" ]; then
-    if [ "$2" == 0 ]; then
+    if [[ $2 -eq 0 ]]; then
         rmdir TrynkitEsp32ISP
-        git clone https://github.com/wezleysherman/TrynkitEsp32ISP
+        git clone https://github.com/wezleysherman/Trynkit-Firmware-Updater
         cd TrynkitEsp32ISP
         pio run
         cd ../
-        python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x130000 /root/TrynkitEsp32ISP/.pioenvs/esp32doit-devkit-v1/firmware.bin
-	else
+        python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x130000 /root/Trynkit-Firmware-Updater/.pioenvs/esp32doit-devkit-v1/firmware.bin
+    else
         python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x130000 $2
     fi
 elif [ "$1" == "firmware" ]; then
-    if [ "$2" == 0 ]; then
+    if [[ $2 -eq 0 ]]; then
         rmdir TrynkitEsp32ISP
         git clone https://github.com/wezleysherman/TrynkitEsp32ISP
         cd TrynkitEsp32ISP
