96256817be6797bb810bde10b18b89d039d054ca
David Anderson <g3muse@gmail.com>
2019-05-20 20:31:32 -0700

    update flashlocal usage message

--- buildscripts/flashlocal.sh
+++ buildscripts/flashlocal.sh
@@ -6,5 +6,5 @@ elif [ "$1" == "firmware" ]; then
     pio run
     python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x130000 $2
 else
-    echo "Invalid parameter(s), try flash.sh [\"bootloader\", \"partitiontable\", \"updater\", \"firmware\"] <path_to_file.bin>"
+    echo "Invalid parameter(s), try flash.sh [\"updater\", \"firmware\"] <path_to_file.bin>"
 fi
