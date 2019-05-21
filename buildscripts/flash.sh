09bea631472299422fe19cfc67f206e1a7499b27
David Anderson <g3muse@gmail.com>
2019-05-20 17:34:50 -0700

    biggest typo in the history of all humanity

--- buildscripts/flash.sh
+++ buildscripts/flash.sh
@@ -10,7 +10,7 @@ if [ "$1" == "bootloader" ]; then
 elif [ "$1" == "partitiontable" ]; then
     if [[ $2 -eq 0 ]]; then
         rm -rf TrynkitEsp32ISP
-        git clone https://$(cat /root/.GITHUBTOKEN)E@github.com/wezleysherman/TrynkitEsp32ISP
+        git clone https://$(cat /root/.GITHUBTOKEN)@github.com/wezleysherman/TrynkitEsp32ISP
         python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x8000 /root/TrynkitEsp32ISP/binary_partitions.bin
     else
         python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x8000 $2
