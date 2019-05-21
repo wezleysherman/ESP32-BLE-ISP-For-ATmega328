fbe5220b38bfcdfc9ec66495db461a7b2c317745
David Anderson <g3muse@gmail.com>
2019-05-20 17:29:13 -0700

    add .GITHUBTOKEN and update readme

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-1. Create CentOS VM with internet access and no user (just root).
-2. Run setup.sh to install dependencies.
-3. Pass through serial USB device to the VM.
-4. Run flash.sh with appropriate option to clone, compile, and flash the partition.
+1. Create CentOS 7 VM with internet access and no user (just root).
+2. In the Setup, make sure ethernet is set to ON
+3. Get the buildscripts onto the VM in a way of your choosing (SFTP, Filezilla, etc)
+4. Setup a Git token  
+    * Go to Github
+    * Go to Settings > Developer Settings > Personal Access Tokens
+    * Click "Generate a New Token" and enter your password again
+    * Set a description/name for it, check the "repo" permission and hit the "Generate token" button at the bottom of the page.
+    * __*Copy your new token before you leave the page*__
+    * Create a new file on your VM: `/root/.GITHUBTOKEN` 
+    * Paste the token and save
+4. Run setup.sh to install dependencies.
+5. Pass through serial USB device to the VM.
+6. Run flash.sh with appropriate parameters. This will clone, compile, then flash the partition.
 
 From fresh CentOS:  
 `cd /buildscripts`  
--- buildscripts/flash.sh
+++ buildscripts/flash.sh
@@ -2,7 +2,7 @@
 if [ "$1" == "bootloader" ]; then
     if [[ $2 -eq 0 ]]; then
         rm -rf Trynkit-Firmware-Updater
-        git clone https://github.com/wezleysherman/Trynkit-Firmware-Updater
+        git clone https://$(cat /root/.GITHUBTOKEN)@github.com/wezleysherman/Trynkit-Firmware-Updater
         python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 /root/Trynkit-Firmware-Updater/bootloader.bin
     else
         python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 $2
@@ -10,7 +10,7 @@ if [ "$1" == "bootloader" ]; then
 elif [ "$1" == "partitiontable" ]; then
     if [[ $2 -eq 0 ]]; then
         rm -rf TrynkitEsp32ISP
-        git clone https://github.com/wezleysherman/TrynkitEsp32ISP
+        git clone https://$(cat /root/.GITHUBTOKEN)E@github.com/wezleysherman/TrynkitEsp32ISP
         python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x8000 /root/TrynkitEsp32ISP/binary_partitions.bin
     else
         python /root/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x8000 $2
@@ -18,7 +18,7 @@ elif [ "$1" == "partitiontable" ]; then
 elif [ "$1" == "updater" ]; then
     if [[ $2 -eq 0 ]]; then
         rm -rf Trynkit-Firmware-Updater
-        git clone https://github.com/wezleysherman/Trynkit-Firmware-Updater
+        git clone https://$(cat /root/.GITHUBTOKEN)@github.com/wezleysherman/Trynkit-Firmware-Updater
         cd TrynkitEsp32ISP
         pio run
         cd ../
@@ -29,7 +29,7 @@ elif [ "$1" == "updater" ]; then
 elif [ "$1" == "firmware" ]; then
     if [[ $2 -eq 0 ]]; then
         rm -rf TrynkitEsp32ISP
-        git clone https://github.com/wezleysherman/TrynkitEsp32ISP
+        git clone https://$(cat /root/.GITHUBTOKEN)@github.com/wezleysherman/TrynkitEsp32ISP
         cd TrynkitEsp32ISP
         pio run
         cd ../
